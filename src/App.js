4261bb409cabdb5c6cef3ec8b3293f2f0872f6fe
Le Truong An <anletr97@gmail.com>
2021-07-24 17:35:23 +0700

    feat: Init todo project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,44 @@
-import './App.css';
-
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import { useState } from "react";
 function App() {
-  return (<div>haha</div>
+  const [tasks, setTasks] = useState([
+    {
+      id: "1",
+      text: "ABC",
+      day: "2021/07/24",
+      reminder: true,
+    },
+    {
+      id: "2",
+      text: "DEF",
+      day: "2021/07/24",
+      reminder: true,
+    },
+    {
+      id: "3",
+      text: "GHK",
+      day: "2021/07/24",
+      reminder: true,
+    },
+  ]);
+  // Delete task on click
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+  // Toggle reminder
+  const toggleReminder = (id) => {
+    console.log(id);
+  }
+  return (
+    <div className="container">
+      <Header />
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>
+      ) : (
+        "Congratz! No tasks left, add some more."
+      )}
+    </div>
   );
 }
 
